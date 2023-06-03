8f077e0ff6aeb4c344f6e8d17e6444a7e6a48e0d
felip011 <79758432+felip011@users.noreply.github.com>
2023-06-03 00:50:05 +0000

    nova pagina

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+const link_img_coraçao = 'https://images.wallpaperscraft.com/image/single/heart_surface_love_85684_480x854.jpg'
+
+function Home(){
+    return(
+    <>
+    <h1 style={{textAlign: 'center', color: '#d81e5b'}}>Te amo irmã voce é muito especial.</h1>
+    <h3 style={{textAlign: 'center'}}>🥰🥰🥰🥰</h3>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src={link_img_coraçao}/>
+    </div>
+    </>
+    
+    
+    ); 
+}
+
+
+
+export default Home;
